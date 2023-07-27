3207b75216abeab76e19c3ece7a1b494e8497a5f
ishigoyal30 <129269261+ishigoyal30@users.noreply.github.com>
2023-07-27 22:47:30 +0530

    Add files via upload

--- Library.py
+++ Library.py
@@ -4,14 +4,18 @@ conn = mysql.connector.connect(
    user='root', password='', host='127.0.0.1', database='ishita_database'
 )
 cursor = conn.cursor()
-cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+cursor.execute("DROP TABLE IF EXISTS employee")
 
-sql ='''CREATE TABLE EMPLOYEE(
+sql ='''CREATE TABLE `employee`(
    USERNAME CHAR(20) NOT NULL,
    PASSWORD CHAR(20) ,
    STATUS CHAR(20),
-   INTIAL_DATE DATETIME
+   INITIAL_DATE DATETIME NOT NULL
 )'''
+sql1='''INSERT INTO `employee`(
+   `USERNAME`,`PASSWORD`,`STATUS`,`INITIAL_DATE`)
+   VALUES('ishi70','goyal30','book issued',NOW())'''
 cursor.execute(sql)
-
+cursor.execute(sql1)
+cursor.execute("commit")
 conn.close()
